59fff62877bae38fb2641df38e1b24e3df057301
sepclouds <sepclouds@gmail.com>
2026-07-13 17:42:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ window.Config = {
 
   // [v2.1] 自定义 CSS (可选)
   // 例如: #footer { display: none; }
-  CustomCSS: '',
+  CustomCSS: '#footer { display: none; }',
 
   // [v2.1] 自定义 JS (可选)
   CustomJS: '',
